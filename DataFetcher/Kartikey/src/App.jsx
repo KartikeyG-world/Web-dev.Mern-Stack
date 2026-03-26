80ff8a27e880bd7fa8e64c41b593251dde36a764
KartikeyG-world <kg704925@gmail.com>
2026-03-26 12:52:08 +0530

    Data Fetcher Through React

---
+++ DataFetcher/Kartikey/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import DataFethcher from './components/DataFethcher'
+
+
+function App() {
+  
+
+  return (
+    <div id='dabba'>
+      <DataFethcher/>
+    </div>
+  )
+}
+
+export default App
