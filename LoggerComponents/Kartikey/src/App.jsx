b5840f43e221b134e9c00c9abef4e076f9b80066
Kartikey <kg704925@gmail.com>
2026-03-27 20:35:52 +0530

    Logger or Increment count with react

---
+++ LoggerComponents/Kartikey/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoggerComponent from './components/LoggerComponent'
+function App() {
+  
+
+  return (
+    <div id='dabba'>
+      <LoggerComponent/>
+    </div>
+  )
+}
+
+export default App
